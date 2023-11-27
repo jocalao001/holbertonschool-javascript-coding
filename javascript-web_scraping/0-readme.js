d8ec6c764a7be438cc94e8125544e868e23d4af2
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-26 23:06:50 -0500

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,17 @@
 #!/usr/bin/node
 
-fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(data);
-  }
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+const read = fs.createReadStream(filePath, 'utf-8');
+
+// Imprimir algun error generado
+read.on('error', (error) => {
+  console.error(error);
+});
+
+// Imprimir la data si no hay error
+read.on('data', (data) => {
+  console.log(data);
 });
