1e6ce1d81f0be42d73fd7fdd2cf3c22c3a3ab53f
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-28 00:24:00 -0500

    Update 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
-
-const fs = require('fs');
-const file = process.argv[2];
-
-fs.readFile(file, 'utf-8', (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+// reads and prints the content of a file
+let fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) { console.log(err); } else { process.stdout.write(data); }
 });
