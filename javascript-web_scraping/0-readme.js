e9a9dbff9723f7a62e0f487d2d10a656e334a669
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-29 22:49:07 -0500

    Fix some errors

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-
 const fs = require('fs');
+const util = require('util');
+
+function readAndPrintFileContent (filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(util.inspect(err, false, null, true));
+      return;
+    }
+    console.log(data);
+  });
+}
 
 const filePath = process.argv[2];
-
-const read = fs.createReadStream(filePath, 'utf-8');
-
-// Imprimir algun error generado
-read.on('error', (error) => {
-  console.error(error);
-});
-
-// Imprimir la data si no hay error
-read.on('data', (data) => {
-  console.log(data);
-});
+readAndPrintFileContent(filePath);
